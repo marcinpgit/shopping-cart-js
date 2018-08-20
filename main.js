18288e1a148b45594c59ae9ba185a190fe9c79cd
marcinpgit <marcinp2012@gmail.com>
2018-08-20 21:07:22 +0200

    incresing quantity of products

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 'use strict';
 
-const cart =[];
+let cart = [];
 const cartDOM = document.querySelector('.cart');
-const addToCartButtonsDOM = document.querySelectorAll('[data-action = "ADD_TO_CART"]');
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
 
 addToCartButtonsDOM.forEach(addToCartButtonDOM => {
-    addToCartButtonDOM.addEventListener('click', () => {
-        const productDOM = addToCartButtonDOM.parentNode;
-        const product = {
-            image: productDOM.querySelector('.product__image').getAttribute('src'),
-            name: productDOM.querySelector('.product__name').innerText,
-            price: productDOM.querySelector('.product__price').innerText
-        };
-        console.table(product);
+  addToCartButtonDOM.addEventListener('click', () => {
+    const productDOM = addToCartButtonDOM.parentNode;
+    const product = {
+      image: productDOM.querySelector('.product__image').getAttribute('src'),
+      name: productDOM.querySelector('.product__name').innerText,
+      price: productDOM.querySelector('.product__price').innerText,
+      quantity: 1,
+    };
 
-        const isInCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
+    const isInCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
 
-        if(!isInCart) {
-            cartDOM.insertAdjacentHTML('beforeend', `
+    if (!isInCart) {
+      cartDOM.insertAdjacentHTML('beforeend', `
         <div class="cart__item">
-            <img class="cart__item__image" src="${product.image}" alt="${product.name}">
-            <h3 class="class__item__name">${product.name}</h3>
-            <h3 class="class__item__name">${product.price}</h3>
+          <img class="cart__item__image" src="${product.image}" alt="${product.name}">
+          <h3 class="cart__item__name">${product.name}</h3>
+          <h3 class="cart__item__price">${product.price}</h3>
+          <button class="btn btn--primary btn--small" data-action="DECRESE_ITEM">&minus;</button>
+          <h3 class="cart__item__quantity">${product.quantity}</h3>
+          <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+          <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
         </div>
-        `);
+      `);
 
-        cart.push(product);
-        addToCartButtonDOM.innerText = 'In Cart';
+      cart.push(product);
+      addToCartButtonDOM.innerText = 'In Cart';
 
-        console.log(cart)
+      const cartItemsDOM = cartDOM.querySelectorAll('.cart__item');
+      cartItemsDOM.forEach(cartItemDOM => {
+        if (cartItemDOM.querySelector('.cart__item__name').innerText === product.name) {
+          cartItemDOM.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', () => {
+            cart.forEach(cartItem => {
+              if (cartItem.name === product.name) {
+                cartItemDOM.querySelector('.cart__item__quantity').innerText = ++cartItem.quantity;
+              }
+            });
+          });
         }
-    }); 
+      });
+    }
+  });
 });
-
